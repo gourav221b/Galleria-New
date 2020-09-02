876c6d3d3af393fa9cbfe3a29461af503fe9cfa4
gourav221b <57912157+gourav221b@users.noreply.github.com>
2020-09-02 12:56:32 +0530

    code uploaded

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const navbar = document.querySelector('.navbar');
 const active = document.querySelector('.active');
 const navContainer = document.querySelector('#navbar');
+navContainer.innerHTML = ' <div class="hamburger-menu" id="ham"><span class="bars"></span></div><div class="brand" onclick="location.replace(' + "/ index.html" + ')"><span>Galleria De Artiste</span></div><div class="menu"><ul class="menu_items"><li><a class="nav_li" href="/index.html" id="home">HOME</a></li><li><a class="nav_li" href="/path/catalog.html" id="catalog">CATALOG</a></li><li><a class="nav_li" href="/path/about.html" id="about">ABOUT</a></li><li><a class="nav_li " href="/path/contact.html" id="contact">CONTACT</a></li><li><a class="nav_li " href="/path/getfeatured.html" id="featured">GET FEATURED</a></li><li class="social"><a href="https://www.facebook.com/GalleriadeArtiste"><i class="fa fa-facebook" aria-hidden="true"></i></a><a href="https://www.instagram.com/galleria_de_artiste/"><i class="fa fa-instagram" aria-hidden="true"></i></a><a href="https://www.reddit.com/r/Galleria_de_artiste/"><i class="fa fa-reddit-alien" aria-hidden="true"></i></a><span id="copyright">&copy; GALLERIA-DE-ARTISTE 2020</span></li></ul></div>';
+
 
 const footer = document.querySelector('.footerContainer');
 footer.innerHTML = ' <div id="shareit"></div> <hr><footer><section class="left_footer"><p id="first">GALLERIA-DE-ARTISTE</p><p>Galleria de artiste is an open platform, multi talent platform featuring Art, Photography, Creative Writings and Music. It is more than just a website featuring art. It is a community, a child born out of the grievances of many new creators        over the unavailability of exclusive platforms for their work.</p></section><section class="middle_footer"><p id="first">Quick Links</p><p><ul class="middle_ul"><li><a href="index.html" class="yellow">Home</a></li>            <li><a class="yellow" href="/path/catalog.html">Catalog</a><ol class="middle_ul"><li><a class="red" href="/path/catalog/art.html">Art</a></li><li><a class="red" href="/path/catalog/photography.html">Photography</a></li><li><a class="red">Writings</a></li>          <li><a class="red">Music</a></li></ol></li><li><a href="/path/about.html" class="yellow">About</a></li>           <li><a href="/path/contact.html" class="yellow">Contact</a></li></ul></p></section><section class="right_footer">   <p id="first">Follow Us on</p><p class="social_media"><div class="social_icon facebook"><a target="_blank" href="https://www.facebook.com/GalleriadeArtiste"><i class="fa fa-facebook" aria-hidden="true"></i></a></div>      <div class="social_icon instagram"><a target="_blank" href="https://www.instagram.com/galleria_de_artiste/"><i class="fa fa-instagram" aria-hidden="true"></i></a></div><div class="social_icon reddit"><a target="_blank" href="https://www.reddit.com/r/Galleria_de_artiste/"><i class="fa fa-reddit-alien" aria-hidden="true"></i></a></div><div class="social_icon twitter"><a><i class="fa fa-twitter" aria-hidden="true"></i></a></div></p><p id="copyright">© Galleria-De-Artiste 2020</p></section></footer>';
@@ -15,13 +17,13 @@ const goto = document.querySelector(".goto");
 function scrollFunction() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         document.getElementById("navbar").style.background = "#000000e5";
-        active.style.color = "#f51707d2";
-        active.style.border = "2px solid transparent";
+        // active.style.color = "#f51707d2";
+        // active.style.border = "2px solid transparent";
         goto.style.display = "inline-block";
     } else {
         document.getElementById("navbar").style.background = "transparent";
-        active.style.color = "#ffffff";
-        active.style.border = "2px solid #ffffff";
+        // active.style.color = "#ffffff";
+        // active.style.border = "2px solid #ffffff";
         goto.style.display = "none";
     }
 }
