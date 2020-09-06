a6bcf8256e78eeaceace62101429899a22fae919
gourav221b <57912157+gourav221b@users.noreply.github.com>
2020-09-06 21:08:54 +0530

    bootstrap glitch

---
+++ js/poems.js
@@ -0,0 +1,79 @@
+var poems = [{
+        "Name": "Love",
+        "Author": "Gourav Ghosal",
+        "Date": "08 Jun 2020",
+        "content": "What is love, the question comes to my mind,<br> Whose answer I still try to find.<br> Is it only an abstract, that <br> Can keep you off your track?<br> Or is it a feeling that two friends share<br> Even when nothing is there.<br>                        I dont know ,what you mean by love.<br> Because still, I can't control my heart.<br> Is love something,that changes your nature<br> By the beauty of the persons personal features? <br> Or is it something that makes you life, <br> It is the choice of the Almighty's dice.<br> Love is a feeling that most of us mistake <br> For passion towards others, that you say.</p><p><br>You dont know what is love,<br> As it is something you've not got.<br> Is love only beside some lakes?<br> Or under Victoria's full grown length? <br> Or is it in your mind's deepest corners,<br> That is impervious- an unbreakabledimension.<br> Love is something you won't understand <br> As you have not seen it nor observed.<br> I know, what is love, because I have seen it,<br> Observed and felt it.</p><p>When everything else was doomed, it <br> Was love that had my life bloomed.<br> It was love that gave me power, <br> When everything else was washed out of my area.<br> There is a difference between love and affair, <br> I wont tell you as you don't care.<br> I love someone who perhaps loves me not, but perhaps hates me,<br> Anyway fate is in my favour.<br> If she loves me ,I am in her heart.<br> If she hates me, I am in her mind.<br> The answer as to what is love, is <br> Something you can never find.<br>"
+    },
+    {
+        "Name": "Yaarana",
+        "Author": "Rajroopa Karmakar",
+        "Date": "08 Jun 2020",
+        "content": "Zindagi bekaar padi thi, <br> Tum doston ne sudhara hai,<br> Haari hui baazi mein,<br> Tum yaaron ka sahaara hai,<br> Jahan ishq ke wo ghum the,<br> Wahan zindagi ka paath padhaya hai,<br> Seedhi saadi thi zindagi,<br> Yaaron ne ise bigada hai,<br> Jab udaasi chaayi thi zindagi mein,<br> Yaaron ne use samhaala hai,<br> Khamoshiyon ke pal ko,<br> Tum sabhi ka hawala hai.</p><p>Apni zindagi ke kuchh alag hi usool he,<br> Dosti Ki khatir hamein<br> Kante bhi kabool hain,<br> Hans kar Chal denge <br> Kaanch ke tukdo par bhi,<br> Agar dost Kahe yeh Dosti<br> Mein bichhaye phool hain.</p><p>Chand se jab mulakat hoti hai,<br> aap logon ke baare mei baat hoti hain,<br> woh Kehte hain mere pas khubsurat sitare hain,<br> Hum Kehte hain unse bhi khubsurat Yaar mera hain!!<br> Dost chahe Kitna bhi boora ho jaye,<br> usse Dosti kabhi mat todo kyun Ki<br> pani chahe Kitna bhi gandha ho jaye,<br> aag bujhane ke kaam ati hai!!<br>"
+    },
+    {
+        "Name": "Secrets to Hide",
+        "Author": "Gourav Ghosal",
+        "Date": "08 Jun 2020",
+        "content": "What we see is false,<br> What we hear are rumours and laments,<br> What we feel has no truth<br> Because everyone have their secrets to hide.</p><p>You think you know someone.<br> You think she is the one.<br> But seldom do you know that <br> Even she has her secrets.</p><p>You feel sad ,you are down,<br> You think you are an open book,<br> But even you yourself have your own <br> Secrets to hide.</p><p>Life can loose all its meaning, <br> Speech can loose its strength, <br> The body will slowly grow weak,<br> And you will be all alone.<br> Your mind will wander off,<br> And will hardly ever return.</p><p>Life may seem strange,<br> Everyone you know will be a stranger,<br> You will think you dont care,<br> But it is you who cares the most.</p><p>Every single man has a mask on,<br> You can't judge who is who,<br> No one will be there by your side <br> Because, everyone has their secrets to hide."
+    },
+    {
+        "Name": "Girl in the Corner",
+        "Author": "Manila Boipai",
+        "Date": "08 Jun 2020",
+        "content": "She sat in the corner,<br> Visible to all,<br> Yet unnoticed.<br><em>Why is she always alone?</em><br> Some wondered.</p><p> She came and went,<br> Sitting silently, talking to no one.<br>        <em>Why doesn’t she talk to anyone?</em><br> Some wondered.</p>    <p>She kept to herself,<br> Had an impassive face.<br> She was not popular,<br> Neither a social outcast.<br> But she was not approached,<br> Neither was she asked for help.<br><em>Is she arrogant?<br> Does she think herself to be superior?<br>Is she proud and haughty?<br>Does she have an ego?</em><br> Everyone wondered.</p><p>One day, like everyday,<br> She came, sat in silence.<br> Impassive face and alone,<br> She sat in her corner,<br> And she sat went, like always,<br> Unnoticed.<br> Next day, she had become Invisible,<br> And then noticed.<br> Everyone wondered,<br> Why she did it?<br><em>Have we made her do it?<br> Did she really wanted to do it?</em></p><p>Slowly past images of her flowed,<br> Memories were shared,<br> She was being remembered.<br> Thoughts voiced,<br> Views opined,<br> Now she was Remembered,<br> And Noticed,<br> But after Death took her away.<br>"
+    },
+    {
+        "Name": "Happy World Environment Day?",
+        "Author": "Shrusti Swain",
+        "Date": "08 Jun 2020",
+        "content": "What did she ask for?<br> A thing as simple as food.<br> Humans betrayed while she trusted us;<br> Her death we wnsured.</p><p>The beach is a major attraction.<br> Identified with travellers and tourists,<br> But recall the dead birds and fish there<br> The mass killings by plastics.</p>        <p>Down, in the forests and meadows;<br> Away, away from towns and ciites,<br> They never endorse or oppose<br> Still suffer from our atrocities.</p><p>Their homes are cleared, cousins wiped out<br> Hundreds face death.<br> And now the world’s biodiversity is struggling<br> In the environment to retain it’s breath.</p><p>This day would be forgotten tomorrow,<br> Many more will come and go too.<br> Yet all and sundry would do nothing<br> Except wish ‘Happy Environment Day’ to you!<br>"
+    },
+    {
+        "Name": "Long and winding <br>roads of life",
+        "Author": "Rajroopa Karmakar",
+        "Date": "08 Jun 2020",
+        "content": "Life is a journey of long and winding roads. <br> Visibility can be poor or non existent at times, of what lies ahead.<br> But it doesn't mean there is no way ahead!<br> The landscapes keep on changing.<br> Life is a journey of long and winding roads.<br> Visibility can be poor or non existent at times,<br> of what lies ahead. <br> But it doesn't mean there is no way ahead!<br> The landscapes keep on changing.<br> Each turn has the promise of taking us nearer,<br> and nearer to our destination. <br> Yet, it is the journey that will take us,<br> to our destination and therefore worth relishing every bit of it.<br>"
+    },
+    {
+        "Name": "The girl in blue",
+        "Author": "Gourav Ghosal",
+        "Date": "08 Jun 2020",
+        "content": "She was beautiful in her own way.<br> But sadly, I thought she was 'The One'<br> My unique nature seemed absurd to her.<br> Not her fault though.</p> <p>I used to over react.<br> Didn't know what to do.<br> Dramatics was my field of art and<br> Silence- her's.<br> A coward- that what she called herself.<br> Braggart- what she called me.</p><p>She never knew how I felt.<br> Didn't hope that to be.<br> I had feelings,<br> Long ago though.<br> Now I wanted friendship.</p> <p>Tried to explain that to her.<br> 'Sorry' she thought.<br> And now out of oblivion, she doesn't speak to me.<br> And me. Being here, and no where to be.</p><p>As I sit here, wondering what the hell to do,<br> 'Writing a poem' is all that I am compelled to do.</p><p>'Goodbye' should I say or stay just quiet.</p><p>Help me out o'readers, cause I am in need of some."
+    },
+    {
+        "Name": "ENDING",
+        "Author": "Gourav Ghosal",
+        "Date": "08 Jun 2020",
+        "content": "I read quotes, I read tales,<br> About heartbreak.<br> I always thought, I could relate.<br> Bluff. It was.<br> Just because I had a crush,<br> never meant I was to be sad.<br> But now, maybe I am.<br> When I heard news, I was supposed to be angry,<br> I was supposed to be sad.<br> Rage was supposed to flow through my veins.<br> But.<br> But.<br> A Strange calmness reigned over.<br> I tried to get angry, But peace suddenly triumphed .<br> It felt numb. But even after everything,<br> I tell myself. 'That was an end to another tale. A tale well told, well spread.'<br> Maybe, we all pick the wrong book,<br> Have wrong idea, frame the wrong stories.<br> Start of well, end....there is none.<br> The book never ends, it's just another<br> Chapter that comes to an end.<br> Or does it?<br>"
+    },
+    {
+        "Name": "Glimpse",
+        "Author": "Gourav Ghosal",
+        "Date": "08 Jun 2020",
+        "content": "A small glimpse, at her soul<br> captured his mind.<br> A little teasing from a friend,<br> tempted his heart.<br> His brain warned him,<br> but emotions overpowered.<br> Past experiences knocked on door,<br> he had already changed address. <br> He was stupid. An idealist.<br> Sentimental fool. He claimed to be<br> Sherlock. But just was John Watson.<br> That girl in the corner.<br> She seemed alone. She was<br> beautiful. Looked as though she <br> needed help. From a friend.<br> But he was stupid then.<br> Maybe he is stupid still.<br> But his heart was clear.<br> His objective simple.<br> But she had a different view.<br> She was told that he was the<br> worse. He didn't know.<br> He never knew.<br> He never knew.<br>"
+    },
+    {
+        "Name": "Unachieved Ascention",
+        "Author": "Gourav Ghosal",
+        "Date": "08 Jun 2020",
+        "content": "One more step was all it needed,<br> One more breath was all it took,<br> We failed still miserably,<br> For we left it too soon.<br> We thought it was impossible,<br> We had no idea how close we were,<br> And thats the thing we never thought <br> One more step was all it had.<br> Just another strike would win,<br> Just another step it should take,<br> Same mistakes were repeated again<br> We left it too soon.<br> Hope lost, despair crept in,<br> One round of strength was all we need.<br> Hands worn out, feet all burnt,<br> Faces were down, as tears fell down.<br> There was this chap, so happy and lively,<br> He was all, but a little late,<br> He came in and saw us weary,<br> “get up you guys, the goal is so close”<br> He said so dearly.<br> We were convinced it wasn’t the truth,<br> Bravery though was our attribute,<br> One more step was all it took,<br> One more blast was all we needed.<br> We celebrated the ascension,<br> We rejoiced at our success,<br> Unable to understand, what it truly meant,<br> What meant to be motivated.<br>"
+    },
+    {
+        "Name": "The True You",
+        "Author": "Gourav Ghosal",
+        "Date": "08 Jun 2020",
+        "content": "You look into the mirror, hoping to see the better you,<br> You relate to characters of fiction, hoping to strengthen you.<br> You put your faith in God, realising He is the true one.<br> You look at yourself, wanting to do better.</p><p> You think you’re the best, also that you’re no good.<br> You get mixed feelings, by none other than you.<br> You look at people, you browse, through everything,<br> Hoping to get some inspiration, but no luck.<br> Maybe it’s not your day, maybe it’s a bad year.<br> All you need probably is a good win.</p><p>There’s a thing, that you don’t realize, <br> The fire in you is real.<br> You tame it, like it’s fake, like you need a real one.<br> All you look for in the mirror, is Validation.<br> The fire burns, in all its glory, waiting to erupt.<br>                        The ink in your pain dries eventually, <br> Hoping you will pick it up.<br> The typewriter is now dusty, all it needs is a brush<br> The one’s validation you seek for, isn’t the one you should trust.</p> <p>So look in the mirror, see for yourself, Look for who you really are,<br> Some validation needing person, or the strong one with the fire .<br> Rise up and shine, for the world hasnt come to an end.<br> All you should need is to trust a true friend.<br>"
+    },
+
+]
+
+myFunction(poems);
+
+function myFunction(arr) {
+    var out = "";
+    var i;
+    for (i = 0; i < arr.length; i++) {
+        out += '<div class="article_row"><p id="first" style="display: flex;">' + arr[i].Name + '</p><p class="tags"><span>' + arr[i].Author + ' </span><span>' + arr[i].Date + '</span></p><div class="col"><p>' + arr[i].content + '</p></div></div><hr>';
+    }
+    document.getElementById("poemDiv").innerHTML = out;
+}
